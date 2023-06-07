fc4e3f5d1af07798ebb318aa246312c353104689
Bedaa1233 <ameliasiregaramel@gmail.com>
2023-06-08 00:09:12 +0700

    done

--- src/components/cardproduct/CardProduct.jsx
+++ src/components/cardproduct/CardProduct.jsx
@@ -4,7 +4,7 @@ import './CardProduct.css'
 const CardProduct = (props) => {
   return (
     <div className="pro" onClick={()=>{
-      return alert ("Product " +props.nm+ "dengan harga "+props.hrg+ "merupakan produk dengan kualitas terbaik!!!")
+      return alert ("Product " +props.nm+ " dengan harga "+props.hrg+ " merupakan produk dengan kualitas terbaik!!!")
     }}>
     <img src = {props.gbr} alt={props.nm} className = 'gbr'/>
     <p>{props.nm}</p>
